8b300438a9d50404cb1c90cf52836056b9543c5a
gregwhitaker <gwhitake@gmail.com>
2018-07-22 20:00:48 -0700

    Doing work

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: path.resolve(__dirname, './src/js/index.js'),
     output: {
-        path: path.resolve(__dirname, './build'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
     },
     plugins: [
